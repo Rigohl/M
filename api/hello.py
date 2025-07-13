078ec72b0b42ceb24f01ea36afc53dc244c4ce41
Rigoberto Huston Laredo <rigobertohula@gmail.com>
2025-07-13 12:13:32 +0000

    hello

---
+++ api/hello.py
@@ -0,0 +1,7 @@
+from http.server import BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello Python Vercel!")
